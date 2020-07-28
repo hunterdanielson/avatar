5daba0f590db30de7881e46d93a797c16af5dcea
Hunter Danielson <hunterleedanielson@gmail.com>
2020-07-28 14:44:13 -0700

    listHook and detailHook tests pass

--- src/hooks/DetailHook.js
+++ src/hooks/DetailHook.js
@@ -25,6 +25,5 @@ export const useDetailHook = () => {
     allies,
     enemies
   };
-
 };
 
---
+++ src/hooks/DetailHook.test.js
@@ -0,0 +1,39 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useDetailHook } from './DetailHook';
+
+jest.mock('react-router-dom', () => ({
+  useParams() {
+    return '1';
+  }
+}));
+
+jest.mock('../services/api.js', () => ({
+  getCharacter() {
+    return Promise.resolve(
+      {
+        '_id': '5cf5679a915ecad153ab68c8',
+        'allies': [
+          'Royal Earthbender Guards'
+        ],
+        'enemies': [
+          'Chin'
+        ],
+        'photoUrl': 'https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441',
+        'name': '46th Earth King',
+        'affiliation': ' Earth Kingdom Earth Kingdom Royal Family'
+      }
+    );
+  }
+}));
+
+describe('detail hook test', () => {
+  it('has name state', () => {
+    return act(() => {
+      const { result, waitFor } = renderHook(() => useDetailHook());
+      return waitFor(() => {
+        expect(result.current.name).not.toBeUndefined();
+      });
+    });
+  });
+
+});
